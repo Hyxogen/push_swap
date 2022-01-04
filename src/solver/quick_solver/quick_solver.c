2e4225a8772a99a6ab7e18afab1586f0be843567
Daan Meijer <daansander02@outlook.com>
2022-01-04 16:42:33 +0100

    Split up solve function

--- src/solver/quick_solver/quick_solver.c
+++ src/solver/quick_solver/quick_solver.c
@@ -30,28 +30,41 @@ static void make_ps_object(int* arr, size_t len, t_ps_object* object) {
 	fill_psa(object, arr, len);
 }
 
-t_instruction* solve(int* arr, size_t len, size_t* instrs) {
-	t_ps_object object;
-	t_instruction* solve_instructions, * temp_instr;
-	size_t rough_count, sort_count;
+static t_instruction* quick_rough_sort(t_ps_object* object, int* arr, size_t len, size_t block_size, size_t* count) {
+	t_instruction* instrs;
 	int* arr_cpy;
 
 	arr_cpy = ft_malloc(len * sizeof(int));
 	ft_memcpy(arr_cpy, arr, len * sizeof(int));
 	quick_sort(arr_cpy, len);
 
+	instrs = rough_sort_optimized(object->m_StackA, object->m_StackB, ips_pb, arr_cpy, block_size, count);
+	free(arr_cpy);
+	return (instrs);
+}
+
+static t_instruction* quick_full_sort(t_ps_object* object, size_t* count) {
+	t_instruction* instrs;
+
+	instrs = sort(object->m_StackB, object->m_StackA, ips_pb, count);
+	translate_instructions(instrs, *count, g_InverseInstructions);
+	return (instrs);
+}
+
+t_instruction* solve(int* arr, size_t len, size_t* instrs) {
+	t_ps_object object;
+	t_instruction* solve_instructions, * temp_instr;
+	size_t rough_count, sort_count;
+
 	make_ps_object(arr, len, &object);
 
-	solve_instructions = rough_sort_optimized(object.m_StackA, object.m_StackB, ips_pb, arr_cpy, 100, &rough_count);
-
-
-	temp_instr = sort(object.m_StackB, object.m_StackA, ips_pb, &sort_count);
-	translate_instructions(temp_instr, sort_count, g_InverseInstructions);
+	solve_instructions = quick_rough_sort(&object, arr, len, 100, &rough_count);
+	temp_instr = quick_full_sort(&object, &sort_count);
 	join_instructions(&solve_instructions, rough_count, temp_instr, sort_count);
+
 	*instrs = rough_count + sort_count;
 	free(temp_instr);
 
 	destroy_ps_object(&object, FALSE);
-	free(arr_cpy);
 	return (solve_instructions);
 }
