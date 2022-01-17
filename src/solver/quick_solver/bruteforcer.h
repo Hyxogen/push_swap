36ed643180168bb098e1084796e285c2c366fc8d
Daan Meijer <daansander02@outlook.com>
2022-01-17 12:04:39 +0100

    Norminize bruteforcer.h

--- src/solver/quick_solver/bruteforcer.h
+++ src/solver/quick_solver/bruteforcer.h
@@ -6,15 +6,16 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/17 11:38:26 by dmeijer       #+#    #+#                 */
-/*   Updated: 2022/01/17 11:38:27 by dmeijer       ########   odam.nl         */
+/*   Updated: 2022/01/17 12:04:30 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BRUTEFORCER_H
-#define BRUTEFORCER_H
+# define BRUTEFORCER_H
 
 # include "../instruction.h"
 
-t_instruction	*brute_force(t_ps_object *object, int depth, size_t *instr_count);
+t_instruction	*brute_force(t_ps_object *object,
+					int depth, size_t *instr_count);
 
 #endif
