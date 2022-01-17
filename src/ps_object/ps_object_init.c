cc7fda3f0f07ec95c8756159c01df58ee1a8c1b5
Daan Meijer <daansander02@outlook.com>
2022-01-17 15:56:34 +0100

    Norminize ps_object_init.c

--- src/ps_object/ps_object_init.c
+++ src/ps_object/ps_object_init.c
@@ -6,7 +6,7 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/17 11:38:48 by dmeijer       #+#    #+#                 */
-/*   Updated: 2022/01/17 11:38:48 by dmeijer       ########   odam.nl         */
+/*   Updated: 2022/01/17 15:56:13 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ void
 }
 
 t_ps_object
-	*ps_object_create_empty()
+	*ps_object_create_empty(void)
 {
-	t_ps_object *object;
+	t_ps_object	*object;
 
 	object = ft_safe_malloc(sizeof(t_ps_object));
 	ps_object_init(object, ideque_create_empty(), ideque_create_empty());
