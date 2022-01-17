145375b3e08e23aeab913e350e2f757b73eda4ae
Daan Meijer <daansander02@outlook.com>
2022-01-17 12:03:07 +0100

    Norminize evaluator.h

--- src/solver/quick_solver/evaluator.h
+++ src/solver/quick_solver/evaluator.h
@@ -6,16 +6,17 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/17 11:37:57 by dmeijer       #+#    #+#                 */
-/*   Updated: 2022/01/17 11:37:57 by dmeijer       ########   odam.nl         */
+/*   Updated: 2022/01/17 12:02:59 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EVALUATOR_H
-#define EVALUATOR_H
+# define EVALUATOR_H
 
 # include "distance.h"
 # include <sys/types.h>
 
-t_distance	evaluate(size_t from_pos, size_t from_size, size_t to_pos, size_t to_size);
+t_distance	evaluate(size_t from_pos, size_t from_size,
+				size_t to_pos, size_t to_size);
 
 #endif
