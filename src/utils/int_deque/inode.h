f40496059053f44fd3e8d578bf7c3c46ff7fbec6
Daan Meijer <daansander02@outlook.com>
2022-01-17 11:45:29 +0100

    Norminize inode.h

--- src/utils/int_deque/inode.h
+++ src/utils/int_deque/inode.h
@@ -6,18 +6,18 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/17 11:36:30 by dmeijer       #+#    #+#                 */
-/*   Updated: 2022/01/17 11:36:30 by dmeijer       ########   odam.nl         */
+/*   Updated: 2022/01/17 11:45:07 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INODE_H
-#define INODE_H
+# define INODE_H
 
 typedef struct s_inode
 {
-	struct s_inode *m_tail;
-	struct s_inode *m_head;
-	int m_content;
+	struct s_inode	*m_tail;
+	struct s_inode	*m_head;
+	int				m_content;
 }	t_inode;
 
 void	inode_init(t_inode *node, t_inode *tail, t_inode *head, int val);
