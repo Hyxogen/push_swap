8e95d782cd767aa34bec72c2fc892084949e3f3c
Daan Meijer <daansander02@outlook.com>
2022-01-17 11:43:17 +0100

    Norminize locale.h

--- src/locale.h
+++ src/locale.h
@@ -6,23 +6,23 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/17 11:36:14 by dmeijer       #+#    #+#                 */
-/*   Updated: 2022/01/17 11:36:15 by dmeijer       ########   odam.nl         */
+/*   Updated: 2022/01/17 11:43:07 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LOCALE_H
-#define LOCALE_H
+# define LOCALE_H
 
-#ifndef PS_ERROR_MESSAGE
-#define PS_ERROR_MESSAGE "Error"
-#endif
+# ifndef PS_ERROR_MESSAGE
+#  define PS_ERROR_MESSAGE "Error"
+# endif
 
-#ifndef PS_OK_MESSAGE
-#define PS_OK_MESSAGE "OK"
-#endif
+# ifndef PS_OK_MESSAGE
+#  define PS_OK_MESSAGE "OK"
+# endif
 
-#ifndef PS_KO_MESSAGE
-#define PS_KO_MESSAGE "KO"
-#endif
+# ifndef PS_KO_MESSAGE
+#  define PS_KO_MESSAGE "KO"
+# endif
 
 #endif
