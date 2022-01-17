ed186ce4e1775514a0be81a3af85cc74137a4fe8
Daan Meijer <daansander02@outlook.com>
2022-01-17 11:58:05 +0100

    Norminize quick_solver_internal.h

--- src/solver/quick_solver/quick_solver_internal.h
+++ src/solver/quick_solver/quick_solver_internal.h
@@ -6,16 +6,17 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/17 11:37:48 by dmeijer       #+#    #+#                 */
-/*   Updated: 2022/01/17 11:37:48 by dmeijer       ########   odam.nl         */
+/*   Updated: 2022/01/17 11:57:53 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef QUICK_SOLVER_INTERNAL_H
-#define QUICK_SOLVER_INTERNAL_H
+# define QUICK_SOLVER_INTERNAL_H
 
 # include "../instruction.h"
 # include "../../ps_object/ps_object.h"
 
-t_instruction	*_quick_solve(t_ps_object *object, const int *arr, size_t len, size_t *instr_count);
+t_instruction	*_quick_solve(t_ps_object *object,
+					const int *arr, size_t len, size_t *instr_count);
 
 #endif
