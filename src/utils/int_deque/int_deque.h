520262073bb0d2b11bf88b40831b14a11357fea0
Daan Meijer <daansander02@outlook.com>
2022-01-17 11:41:37 +0100

    Norminize int_deque.h

--- src/utils/int_deque/int_deque.h
+++ src/utils/int_deque/int_deque.h
@@ -6,12 +6,12 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/17 11:36:28 by dmeijer       #+#    #+#                 */
-/*   Updated: 2022/01/17 11:36:28 by dmeijer       ########   odam.nl         */
+/*   Updated: 2022/01/17 11:41:23 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef DEQUE_H
-#define DEQUE_H
+#ifndef INT_DEQUE_H
+# define INT_DEQUE_H
 
 # include "inode.h"
 # include <sys/types.h>
@@ -19,16 +19,17 @@
 
 typedef struct s_deque
 {
-	t_inode *m_front;
-	t_inode *m_back;
-	size_t m_size;
+	t_inode	*m_front;
+	t_inode	*m_back;
+	size_t	m_size;
 }	t_ideque;
 
-void		ideque_init(t_ideque *deque, t_inode *front, t_inode *back, size_t size);
+void		ideque_init(t_ideque *deque, t_inode *front,
+				t_inode *back, size_t size);
 void		ideque_destroy(t_ideque *deque, ft_bool free_self);
 
 /*MALLOC SAFE*/
-t_ideque	*ideque_create_empty();
+t_ideque	*ideque_create_empty(void);
 
 ft_bool		ideque_is_empty(const t_ideque *deque);
 size_t		ideque_get_size(const t_ideque *deque);
