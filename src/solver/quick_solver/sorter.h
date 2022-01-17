f106c358498ebdc57880fde3127903907687416c
Daan Meijer <daansander02@outlook.com>
2022-01-17 11:51:04 +0100

    Norminize sorter.h

--- src/solver/quick_solver/sorter.h
+++ src/solver/quick_solver/sorter.h
@@ -6,12 +6,12 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/17 11:37:30 by dmeijer       #+#    #+#                 */
-/*   Updated: 2022/01/17 11:37:30 by dmeijer       ########   odam.nl         */
+/*   Updated: 2022/01/17 11:50:57 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SORTER_H
-#define SORTER_H
+# define SORTER_H
 
 # include "../instruction.h"
 # include "../../utils/int_deque/int_deque.h"
@@ -20,13 +20,13 @@ typedef size_t(*t_pos_func)(int, t_ideque *);
 
 typedef struct s_sort_info
 {
-	int m_min;
-	int m_max;
+	int				m_min;
+	int				m_max;
 
-	t_pos_func m_pos_func;
-	t_instruction m_put_instr;
-	t_ideque *m_from_deque;
-	t_ideque *m_to_deque;
+	t_pos_func		m_pos_func;
+	t_instruction	m_put_instr;
+	t_ideque		*m_from_deque;
+	t_ideque		*m_to_deque;
 
 }	t_sort_info;
 
