7413386266a305325adbdb1983633abf0ff2de18
Daan Meijer <daansander02@outlook.com>
2022-01-17 11:47:35 +0100

    Norminize ideque_destroy.c

--- src/utils/int_deque/ideque_destroy.c
+++ src/utils/int_deque/ideque_destroy.c
@@ -6,7 +6,7 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/17 11:37:15 by dmeijer       #+#    #+#                 */
-/*   Updated: 2022/01/17 11:37:15 by dmeijer       ########   odam.nl         */
+/*   Updated: 2022/01/17 11:47:24 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ void
 	ideque_destroy(t_ideque *deque, ft_bool free_self)
 {
 	_ideque_destroy_nodes(deque);
-
 	if (free_self)
 		free(deque);
 }
