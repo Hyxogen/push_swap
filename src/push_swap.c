9662a1a2907a2bac694eed33f0fb28b2f5d204d9
Daan Meijer <daansander02@outlook.com>
2022-01-17 11:42:49 +0100

    Norminize push_swap.c

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/07 13:20:10 by dmeijer       #+#    #+#                 */
-/*   Updated: 2021/12/16 09:28:38 by dmeijer       ########   odam.nl         */
+/*   Updated: 2022/01/17 11:42:37 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,21 @@
 int
 	main(int argc, char **argv)
 {
-	t_instruction *solve_instr;
-	size_t instr_count;
-	int *lst;
+	t_instruction	*solve_instr;
+	size_t			instr_count;
+	int				*lst;
 
 	if (argc <= 1)
 		return (EXIT_SUCCESS);
-
 	lst = read_arguments(argc, argv);
-
 	if (lst == NULL || iarray_has_duplicates(lst, argc - 1))
 	{
 		free(lst);
 		ft_putendl_fd(2, PS_ERROR_MESSAGE);
 		return (EXIT_FAILURE);
 	}
-
 	solve_instr = solve(lst, argc - 1, &instr_count);
 	print_instructions(solve_instr, instr_count);
-
 	free(solve_instr);
 	free(lst);
 	return (EXIT_SUCCESS);
