00679f58f5fdf11a1031bd86b0498ba35e208d98
Daan Meijer <daansander02@outlook.com>
2022-01-17 12:04:13 +0100

    Norminize distance.h

--- src/solver/quick_solver/distance.h
+++ src/solver/quick_solver/distance.h
@@ -6,19 +6,19 @@
 /*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/17 11:38:20 by dmeijer       #+#    #+#                 */
-/*   Updated: 2022/01/17 11:38:20 by dmeijer       ########   odam.nl         */
+/*   Updated: 2022/01/17 12:04:04 by dmeijer       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DISTANCE_H
-#define DISTANCE_H
+# define DISTANCE_H
 
 # include <sys/types.h>
 
 typedef struct s_distance
 {
-	long m_left_dist;
-	long m_right_dist;
+	long	m_left_dist;
+	long	m_right_dist;
 }	t_distance;
 
 int		distance_cmp(const t_distance *a, const t_distance *cmp);
